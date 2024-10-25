4d2e37589603bcd874b6da3e3066206b8626ae30
Styrse <styrbjoern@me.com>
2024-10-25 16:41:05 +0200

    Create DigitalBook.java

---
+++ src/DigitalBook.java
@@ -0,0 +1,19 @@
+public class DigitalBook extends Title{
+
+
+
+    @Override
+    public double calculateRoyalty() {
+        return 0;
+    }
+
+    @Override
+    public double getPoints() {
+        return 0;
+    }
+
+    @Override
+    public double getPages() {
+        return 0;
+    }
+}
