45d43eb92ea1399e64db886ca411630fa17b6bb2
Styrse <styrbjoern@me.com>
2024-10-26 20:10:18 +0200

    Update

--- src/PrintedBook.java
+++ src/PrintedBook.java
@@ -11,9 +11,4 @@ public class PrintedBook extends PhysicalBook{
     protected double calculatePoints() {
         return pages * calculateLiteraturePoints(this) * getCopies();
     }
-
-    @Override
-    protected double calculateLiteraturePoints(Title title) {
-        return super.calculateLiteraturePoints(this);
-    }
 }
